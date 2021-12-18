f9cd43230601249384525a8a1617dcd822712738
Hermann <hermann.kaebi@gmail.com>
2021-12-18 15:11:20 +0200

    Added new blog page version
    New:
    1) Can now save as draft

--- write/index.js
+++ write/index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
-import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-firestore.js";
+import { getFirestore, setDoc, doc, updateDoc, getDoc } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-firestore.js";
 const firebaseConfig = {
   apiKey: "AIzaSyCHExhBICb909d7nRAb9R3Ce0HtZNNZGrw",
   authDomain: "notes-hermannkabi.firebaseapp.com",
@@ -13,9 +13,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get('href');
-console.log(id);
+const id = urlParams.get('id');
 var converter = new showdown.Converter();
+
 
 function makeid(length) {
   var result           = '';
@@ -29,6 +29,28 @@ charactersLength));
 }
 
 
+//Saving and drafts
+//Drafts get their own Firestore collection (e.g. document 'drafts/CODE/')
+//When saving a draft for the first time, create a unique id like for publishing
+//Add that code to URL (e.g /write?id=CODE)
+//When JS detects a code in the URL, it tries to load data from that document
+//Saving a draft again saves it under that code
+
+
+
+async function loadPost(){
+  var docSnap = await getDoc(doc(db, "drafts", id));
+  if(docSnap.exists()){
+    var title = docSnap.data().title;
+    var body = docSnap.data().body;
+
+    $(".title").val(title);
+    $(".post-writer").val(body);
+  }
+}
+
+if(id) loadPost();
+
 async function setPost(title, body){
     var date = Date.now();
     var id = makeid(15);
@@ -40,6 +62,31 @@ async function setPost(title, body){
       postId: id,
     });
 }
+
+async function saveAsDraft(title, body){
+  var date = Date.now();
+  if(id){
+    //Save to that id
+    await updateDoc(doc(db, "drafts", id), {
+      title: title,
+      body: body,
+      timeToRead: 2,
+      date: date,
+      postId: id,
+    });
+    return;
+  }
+  //Create a new id and set the doc
+  var newId = makeid(15);
+  await setDoc(doc(db, "drafts", newId), {
+    title: title,
+    body: body,
+    timeToRead: 2,
+    date: date,
+    postId: newId,
+  });
+  window.location.href = "?id="+newId;
+} 
 
 
 $(".preview").click(function (){
@@ -63,3 +110,27 @@ $(".publish").click(function (){
     window.location.href = "/";
   });
 });
+
+$(".draft").click(function (){
+  saveAsDraft($(".title").val(), $(".post-writer").val());
+});
+
+function checkForScreenSize(){
+  if($(window).width() <= 1000){
+    //Too small to write a blog post, show error page
+    $(".mobile-error").show();
+    $(".wrapper").hide();
+  }else{
+    $(".mobile-error").hide();
+    $(".wrapper").show();
+  }
+}
+
+$(window).resize(function (){
+  checkForScreenSize();
+});
+
+
+
+//This is needed to check the size when the page loads, as resize is not called
+checkForScreenSize();
